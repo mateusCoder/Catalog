bbf1a01a5ddfc261a80082b4ca3cb1de5c6801a6
mateusCoder <mateus.moraes0507@gmail.com>
2022-05-24 16:11:39 -0300

    Add CategoryController

---
+++ backend/catalog/src/main/java/com/ds/catalog/entities/Category.java
@@ -0,0 +1,30 @@
+package com.ds.catalog.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Category implements Serializable {
+
+    private Long id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        Category category = (Category) o;
+        return Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
